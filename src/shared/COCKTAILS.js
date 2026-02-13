73e07722d70097109ec6ecbf6d3f19941d92ce6f
lina98st <156576799+lina98st@users.noreply.github.com>
2026-02-13 20:40:43 +0100

    Add COCKTAILS dummy data

---
+++ src/shared/COCKTAILS.js
@@ -0,0 +1,40 @@
+export const COCKTAILS = [
+  {
+    idDrink: "1",
+    strDrink: "Margarita",
+    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg",
+    strCategory: "Ordinary Drink",
+    strAlcoholic: "Alcoholic",
+    strGlass: "Cocktail glass",
+    strInstructions: "Rub the rim of the glass with the lime slice to make the salt stick to it. Shake the other ingredients with ice, then carefully pour into the glass.",
+    strIngredient1: "Tequila",
+    strIngredient2: "Triple sec",
+    strIngredient3: "Lime juice",
+    strIngredient4: "Salt"
+  },
+  {
+    idDrink: "2",
+    strDrink: "Mojito",
+    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/metwgh1606770327.jpg",
+    strCategory: "Cocktail",
+    strAlcoholic: "Alcoholic",
+    strGlass: "Highball glass",
+    strInstructions: "Muddle mint leaves with sugar and lime juice. Add a splash of soda water and fill the glass with cracked ice. Pour the rum and top with soda water.",
+    strIngredient1: "White rum",
+    strIngredient2: "Lime",
+    strIngredient3: "Sugar",
+    strIngredient4: "Mint"
+  },
+  {
+    idDrink: "3",
+    strDrink: "Pina Colada",
+    strDrinkThumb: "https://www.thecocktaildb.com/images/media/drink/cpf4j51504348579.jpg",
+    strCategory: "Cocktail",
+    strAlcoholic: "Alcoholic",
+    strGlass: "Hurricane glass",
+    strInstructions: "Mix with crushed ice in blender until smooth. Pour into glass and garnish with pineapple slice.",
+    strIngredient1: "White rum",
+    strIngredient2: "Coconut cream",
+    strIngredient3: "Pineapple juice"
+  }
+];
