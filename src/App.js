96008f4a353e989b18f7efd535421c38419254ff
lina98st <156576799+lina98st@users.noreply.github.com>
2026-02-15 19:04:39 +0100

    Add basic App.js structure

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-//import logo from './logo.svg';
-//import './App.css';
-import GameCard from './components/GameCard.js';
-
-
-const gameList = [
-  {
-    id: 0,
-    name: "Hogwarts Legacy",
-    rating: null,
-    category: "fun"
-  },
-    {
-    id: 0,
-    name: "Lord of the Rings",
-    rating: null,
-    category: "fun"
-    },
-      {
-    id: 0,
-    name: "GTA 4",
-    rating: null,
-    category: "fun"
-  }
-]
-
-
+import './App.css';
 
 function App() {
   return (
-<div>
-  <h1>My Board Game</h1>
-{gameList.map( g => <GameCard game={g}/> ) }
-</div>
+    <div className="App">
+      <h1>CocktailFinder</h1>
+    </div>
   );
 }
 
