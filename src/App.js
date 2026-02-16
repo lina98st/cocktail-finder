885941f75cd7ab80e89996ceede4eef13e1dc509
lina98st <156576799+lina98st@users.noreply.github.com>
2026-02-16 21:55:56 +0100

    Add API fetch functionality with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { COCKTAILS } from './shared/COCKTAILS';
 import './App.css';
 
+
 function App() {
+  const [cocktails, setCocktails] = useState(COCKTAILS);
+
+  useEffect(() => {
+    fetchCocktail();
+  }, []);
+
+async function fetchCocktail() {
+  try {
+    let response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    let data = await response.json();
+    console.log(data);
+    setCocktails(data.drinks);
+    } catch (error) {
+    console.error('There was an error', error)
+  }
+}
+
+
   return (
     <div className="App">
       <h1>CocktailFinder</h1>
